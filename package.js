0436998dc44b66db75808f76f865e0da9c4a9570
dm <dmplutenko@gmail.com>
2013-08-01 01:52:12 +0300

    meteor 6

--- package.js
+++ package.js
@@ -2,6 +2,12 @@ Package.describe({
   summary: "Meteor smart package for AWS SDK node.js"
 });
 
+if((typeof Npm==="object")||(typeof Npm==="function")){
+  if (typeof Npm.depends == "function"){
+    Npm.depends({"aws-sdk": "1.4.1"});
+  }
+}
+
 Package.on_use(function (api) {
   api.add_files('aws_server.js', 'server');
 });
